45e4f33051e4aae752262d8b27cc853493a2cd8b
Agenor Ikeda <china.ikeda@gmail.com>
2025-06-03 08:46:34 -0300

    Início do projeto

---
+++ src/test/java/com/ikeda/authuser/MsAuthuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikeda.authuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
