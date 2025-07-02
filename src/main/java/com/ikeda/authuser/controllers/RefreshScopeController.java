70f38b3a28ede30705b988640e2cdc1a560bf44d
Agenor Ikeda <china.ikeda@gmail.com>
2025-07-02 17:56:44 -0300

    ConfigServer e ConfigServerRepo - repositório no git

--- src/main/java/com/ikeda/authuser/controllers/RefreshScopeController.java
+++ src/main/java/com/ikeda/authuser/controllers/RefreshScopeController.java
@@ -1,2 +1,17 @@
-package com.ikeda.authuser.controllers;public class RefreshScopeController {
+package com.ikeda.authuser.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RefreshScopeController {
+
+    @Value(value = "${authuser.refreshscope.name}")
+    private String name;
+
+    @RequestMapping("/refreshscope")
+    public String refreshscope(){
+        return this.name;
+    }
 }
